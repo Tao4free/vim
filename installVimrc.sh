7fdd58d5790432319b604e721335fc4406dda24b
Tao4free <tao4free@gmail.com>
2019-12-04 10:40:46 +0900

    update for bash setting

--- installVimrc.sh
+++ installVimrc.sh
@@ -2,5 +2,9 @@
 
 VIMRC=vimrc-noplugin
 VIMRCPATH="$( cd "$(dirname "$0")" ; pwd -P )"/$VIMRC
+BASHRC=bashrc
+BASHRCPATH="$( cd "$(dirname "$0")" ; pwd -P )"/$BASHRC
 # echo $VIMRCPATH
 cp $VIMRCPATH ~/.vimrc
+cat $BASHRCPATH >> ~/.bashrc
+
